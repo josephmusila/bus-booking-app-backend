a4343aae0e78f662886b92b5c8c25c0c2261e6a6
josephmusila <josemusila03@gmail.com>
2023-11-03 13:06:42 +0300

    deployed the project to cpanel

--- bus/settings.py
+++ bus/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 
 env=environ.Env(DEBUG=(bool,False))
@@ -129,7 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+MEDIA_ROOT=os.path.join(BASE_DIR,"uploads")
+MEDIA_URL = "uploads/"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
